0a09621341f1324eed287084d2eb0a3641658f74
Kerynn Davis <kerynn.davis@gmail.com>
2022-09-04 11:28:33 -0700

    Add puts statements

--- variables2.rb
+++ variables2.rb
@@ -2,3 +2,14 @@ name = "Kerynn"
 pet_name = "Charlie"
 age = 34
 is_nervous = true
+
+
+name = "Jim"
+age = 43
+is_nervous = false
+
+p name
+p is_nervous
+
+puts "My name is #{name} and I am #{age} years old."
+puts "I have a dog named #{pet_name}."
